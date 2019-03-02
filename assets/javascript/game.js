4801833d8d447cd7fb5476f937d75d2adf6d7a72
SBC-VN <vnestohr@juno.com>
2019-03-01 23:04:29 -0500

    Initial selection logic working.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,8 +5,8 @@ var jediArray = [{name:"Darth Vader",strength:0,regen:0,picture:"./assets/images
                  {name:"Count Dooku",strength:0,regen:0,picture:"./assets/images/count_dooku.jfif",status:"ready"},
                  {name:"Darth Maul",strength:0,regen:0,picture:"./assets/images/darth_maul.jpg",status:"ready"},
                  {name:"Luke Skywalker",strength:0,regen:0,picture:"./assets/images/luke_skywalker.jpg",status:"ready"},
-                 {name:"Mace Windu",strength:0,regen:0,picture:"./assets/images/mace_windu.jpg",status:"ready"}, 
-                 {name:"ObiWan Kenobi",strength:0,regen:0,picture:"./assets/images/obiwan_kenobi.jpg",status:"ready"}];
+                 {name:"Mace Windu",strength:0,regen:0,picture:"./assets/images/mace_windu.png",status:"ready"}, 
+                 {name:"Obi Wan Kenobi",strength:0,regen:0,picture:"./assets/images/obiwan_kenobi.jpg",status:"ready"}];
                  
 // Generate somewhat random characteristics.   
 // Range of values: Strength 50 - 150, Regen 25 - 75
@@ -23,11 +23,13 @@ for (var i=0; i<jediArray.length; i++) {
 var attackChoiceElement=$("#attacker-choices");
 var defenderChoiceElement=$("#defender-choices");
 
-function createJediCard (name, strength, regen, imageSrc) {
+function createJediCard (index, name, strength, regen, imageSrc) {
     var newCard = $("<div>");
 
-    newCard.addClass("card mb-3");
-    newCard.attr("style","max-width: 540px;");
+    newCard.addClass("card mb-3 jedi-card");
+    newCard.attr("id", index);
+    newCard.css("max-width","350px");
+    newCard.css("background-color","grey");
 
     var cardRow = $("<div>");
     newCard.append(cardRow);
@@ -35,28 +37,106 @@ function createJediCard (name, strength, regen, imageSrc) {
     
     var rowColumn1 = $("<div>");
     cardRow.append(rowColumn1);
-    rowColumn1.addClass("col-md-4");
+    rowColumn1.addClass("col-md-7");
 
     var cardImage = $("<img>");
     rowColumn1.append(cardImage);
     cardImage.addClass("card-img");
     cardImage.attr("src",imageSrc);
     cardImage.attr("width","100px");
-    cardImage.attr("height","100px");
+    cardImage.attr("height","200px");
 
     var rowColumn2 = $("<div>");
     cardRow.append(rowColumn2);
-    rowColumn2.addClass("col-md-8");
+    rowColumn2.addClass("col-md-5");
 
     var column2Body = $("<div>");
     rowColumn2.append(column2Body);
     column2Body.addClass("card-body");
     column2Body.append("<h5 class=\"card-title\">"+ name +"</h5>");
-    column2Body.append("<p class=\"card-text\">Strength:" + strength +"</p>");
-    column2Body.append("<p class=\"card-text\">Regeneration:" + regen +"</p>");
+    column2Body.append("<p class=\"card-text\">Strength: " + strength +"</p>");
+    column2Body.append("<p class=\"card-text\">Regen: " + regen +"</p>");
 
     return newCard;
 }
 
-var jediCard1 = createJediCard("test",5,5,"./assets/images/obiwan_kenobi.jpg");
-$("#attacker-choices").append(jediCard1);
+function setAttacker(indx) {    
+    $("#attacker-header").text("Attacker");
+    $("#attacker-choices").css("display","none");
+    $("#defender-container").css("display","inline-flex");
+
+    for (i = 0; i < jediArray.length; i++) {
+
+        var card = $("#"+i);
+
+        if (i === indx) {
+            jediArray[i].status = "attacker";
+            $("#attacker-card").append(card);
+        }
+        else {
+             $("#defender-choices").append(card);
+        }
+    }
+}
+
+function setDefender(indx) {    
+    $("#defender-header").text("Defender");
+    $("#defender-choices").css("display","none");
+    $("#remainder-container").css("display","inline-flex");
+
+    for (i = 0; i < jediArray.length; i++) {
+
+        var card = $("#"+i);
+        console.log(i);
+        if (i === indx) {
+            jediArray[i].status = "defender";
+            $("#defender-card").append(card);
+        }
+        else if (jediArray[i].status === "ready") {
+            console.log(jediArray[i].status);
+            $("#remainder-choices").append(card);
+        }
+    }
+}
+
+for (i=0; i<jediArray.length; i++) {
+    var jediCard1 = createJediCard(i,
+                                   jediArray[i].name,
+                                   jediArray[i].strength,
+                                   jediArray[i].regen,
+                                   jediArray[i].picture);
+    $("#attacker-choices").append(jediCard1);
+}
+
+// Hide the defender section until an attacker is selected.
+$("#defender-container").css("display","none");
+// Hide the remaiing section until after both attacker and defender are declared.
+$("#remainder-container").css("display","none");
+
+var selectionMode = "attacker";
+
+$(".jedi-card").on("mouseenter", function() {
+      if (jediArray[parseInt(this.id)].status === "ready") {
+        this.style.backgroundColor = "white";
+    }
+});
+
+$(".jedi-card").on("mouseleave", function() {
+    if (jediArray[parseInt(this.id)].status === "ready") {
+        this.style.backgroundColor = "grey";
+    }
+});
+
+$(".jedi-card").on("dblclick", function() {
+    var jediIndex = parseInt(this.id);
+    if (jediArray[jediIndex].status === "ready") {
+        if (selectionMode === "attacker") {
+            setAttacker(jediIndex);
+            selectionMode = "defender";
+        }
+        else if (selectionMode === "defender") {
+            setDefender(jediIndex);
+            selectionMode = "combat";
+        }
+    }
+});
