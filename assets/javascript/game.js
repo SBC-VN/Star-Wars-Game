36fb007e0f8c6c7b42ff5e00efebdd57172cd316
SBC-VN <vnestohr@juno.com>
2019-03-05 20:08:53 -0500

    Periodic save point.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 
+var jediArray = [];
 var attackingJedi=null;
 var defendingJedi=null;
 
@@ -12,46 +13,137 @@ function cardMouseLeave(evt) {
     this.style.backgroundColor = "grey";
 }
 
-function populateAttackerSelection() {
+function setAttackButtons(jediRec) {
+    $("#attacker-buttons").empty();
 
+    // Aggressive attack
+    var attackValue = Math.floor(jediRec.strength * 0.6 + ((jediRec.strength * 0.2) * Math.random()));
+    jediRec.attacks[0][0] =  Math.floor(attackValue * 0.8);
+    jediRec.attacks[0][1] =  Math.floor(attackValue * 0.2);
+    var button = $("<button type=\"button\" class=\"btn btn-danger button-attack\"  id=\"btn-attack-1\">");
+    button.text("Lunge (" + jediRec.attacks[0][0] + "/" + jediRec.attacks[0][1]+")");
+    $("#attacker-buttons").append(button);
+
+    // Balanced attack
+    attackValue = Math.floor(this.strength * 0.4 + ((this.strength * 0.1) * Math.random()));
+    jediRec.attacks[1][0] =  Math.floor(attackValue * 0.6);
+    jediRec.attacks[1][1] =  Math.floor(attackValue * 0.4);
+    button = $("<button type=\"button\" class=\"btn btn-danger button-attack\"  id=\"btn-attack-2\">");
+    button.text("Slash (" + jediRec.attacks[1][0] + "/" + jediRec.attacks[1][1]+")");
+    $("#attacker-buttons").append(button);
+
+    // Light attack
+    attackValue = Math.floor(this.strength * 0.4 + ((this.strength * 0.1) * Math.random()));
+    jediRec.attacks[2][0] =  Math.floor(attackValue * 0.4);
+    jediRec.attacks[2][1] =  Math.floor(attackValue * 0.6);
+    button = $("<button type=\"button\" class=\"btn btn-danger button-attack\"  id=\"btn-attack-3\">");
+    button.text("Jab (" + jediRec.attacks[2][0] + "/" + jediRec.attacks[2][1]+")");
+    $("#attacker-buttons").append(button);
+    $("#attacker-buttons").css("display","inline-grid");
+    $("#attacker-buttons").css("margin-bottom","1rem");
+}
+
+function setDefendButtons(jediRec) {
+    $("#defender-buttons").empty();
+
+    // Aggressive defense
+    var attackValue = Math.floor(jediRec.strength * 0.6 + ((jediRec.strength * 0.2) * Math.random()));
+    jediRec.defends[0][0] =  Math.floor(attackValue * 0.8);
+    jediRec.defends[0][1] =  Math.floor(attackValue * 0.2);
+    var button = $("<button type=\"button\" class=\"btn btn-danger button-attack\"  id=\"btn-attack-1\">");
+    button.text("Block (?/?)");
+    $("#defender-buttons").append(button);
+
+    // Balanced defense
+    attackValue = Math.floor(this.strength * 0.4 + ((this.strength * 0.1) * Math.random()));
+    jediRec.defends[1][0] =  Math.floor(attackValue * 0.6);
+    jediRec.defends[1][1] =  Math.floor(attackValue * 0.4);
+    button = $("<button type=\"button\" class=\"btn btn-danger button-attack\"  id=\"btn-attack-2\">");
+    button.text("Parry (?/?)");
+    $("#defender-buttons").append(button);
+
+    // Light defense
+    attackValue = Math.floor(this.strength * 0.4 + ((this.strength * 0.1) * Math.random()));
+    jediRec.defends[2][0] =  Math.floor(attackValue * 0.4);
+    jediRec.defends[2][1] =  Math.floor(attackValue * 0.6);
+    button = $("<button type=\"button\" class=\"btn btn-danger button-attack\"  id=\"btn-attack-3\">");
+    button.text("Evade (?/?))");
+    $("#defender-buttons").append(button);
+    $("#defender-buttons").css("display","inline-grid");
+}
+
+function populateAttackerSelection() {
+    $("#attacker-choices").css("display","inline-flex");
+ 
+    for (var i=0; i<jediArray.length; i++) {
+        if (jediArray[i].status === "ready") {
+             $("#attacker-choices").append(jediArray[i].card);
+        }
+    }
 }
 
 function populateDefenderSelection() {
-
+    $("#defender-container").css("display","block");
+    $("#defender-choices").css("display","inline-flex");
+    $("#defender-selected").css("display","block");
+ 
+    for (var i=0; i<jediArray.length; i++) {
+        if (jediArray[i].status === "ready") {
+             $("#defender-choices").append(jediArray[i].card);
+        }
+    }
 }
 
 function populateRemainingSelection() {
-
+    $("#remainder-container").css("display","block");
+    $("#remainder-choices").css("display","inline-flex");
+ 
+    for (var i=0; i<jediArray.length; i++) {
+        if (jediArray[i].status === "ready") {
+             $("#remainder-choices").append(jediArray[i].card);
+        }
+    }
 }
 
-function cardDoubleClick(evt) {
-    
-    if (this.status === "ready") {
+function jediCardDoubleClick(evt) {
+  
+    var jediInfo = jediArray[parseInt(this.getAttribute("id"))];
+
+    if (jediInfo.status === "ready") {
         if (selectionMode === "attacker") {
             $("#attacker-card").empty();
-            $("#attacker-card").append(this.card);
-            this.status = "attacker";
-            attackingJedi = this;
+            $("#attacker-card").append(jediInfo.card);
+            $("#attacker-header").text("Attacker:");
+            //$("#attacker-card-container").css("display","contents");
+            //$("#attacker-choices").empty();
+            jediInfo.status = "attacker";
+            attackingJedi = jediInfo;
             if (defendingJedi == null) {
                 populateDefenderSelection();
                 selectionMode = "defender";
             }
             else {
                 populateRemainingSelection();
+                setAttackButtons(attackingJedi);
+                setDefendButtons(defendingJedi);
                 selectionMode = "none";
             }
         }
         else if (selectionMode === "defender") {
             $("#defender-card").empty();
             $("#defender-card").append(this.card);
-            this.status = "defender";
-            defendingJedi = this;
+            $("#defender-header").text("Attacker:");
+            $("#defender-selected").css("display","contents");
+            jediInfo.status = "defender";
+            defendingJedi = jediInfo;
             if (attackingJedi == null) {
                 populateAttackerSelection();
                 selectionMode = "attacker";
             }
             else {
                 populateRemainingSelection();
+                setAttackButtons(attackingJedi);
+                setDefendButtons(defendingJedi);
                 selectionMode = "none";
             }
         }
@@ -75,7 +167,7 @@ function createJediCard(jediId, jediName, jediImage,strength,regen,hp) {
     newCard.css("background-color","grey");
     newCard.on("mouseenter",cardMouseEnter);
     newCard.on("mouseleave",cardMouseLeave)
-    newCard.on("dblclick",cardDoubleClick)
+    newCard.on("dblclick",jediCardDoubleClick)
     return newCard;
 }
 
@@ -96,21 +188,16 @@ class Jedi {
                   this.strength + this.hp + this.regen > 475);
 
         // Calculate the jedi's attack modes.
-        var attackValue = Math.floor(this.strength * 0.6 + ((this.strength * 0.2) * Math.random()));
+        
         this.attacks = [];
         this.attacks[0] = [];
-        this.attacks[0][1] =  Math.floor(attackValue * 0.8);
-        this.attacks[0][2] =  Math.floor(attackValue * 0.2);
-
-        attackValue = Math.floor(this.strength * 0.4 + ((this.strength * 0.1) * Math.random()));
         this.attacks[1] = [];
-        this.attacks[1][1] =  Math.floor(attackValue * 0.6);
-        this.attacks[1][2] =  Math.floor(attackValue * 0.4);
-
-        attackValue = Math.floor(this.strength * 0.4 + ((this.strength * 0.1) * Math.random()));
         this.attacks[2] = [];
-        this.attacks[2][1] =  Math.floor(attackValue * 0.4);
-        this.attacks[2][2] =  Math.floor(attackValue * 0.6);
+
+        this.defends = [];
+        this.defends[0] = [];
+        this.defends[1] = [];
+        this.defends[2] = [];
 
         this.card = createJediCard( jediId,
                                     this.name,
@@ -123,7 +210,6 @@ class Jedi {
 }
 
 $(document).ready(function() {
-    var jediArray = [];
     jediArray[0] = new Jedi(0, "Darth Vader", "./assets/images/darth_vader.jpg");
     jediArray[1] = new Jedi(1, "Count Dooku", "./assets/images/count_dooku.jfif");
     jediArray[2] = new Jedi(2, "Darth Maul", "./assets/images/darth_maul.jpg");
